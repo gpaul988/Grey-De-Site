6ccc9427f6f12bb9fb72693f05a334ae18993858
gpaul988 <gpaul988@gmail.com>
2025-01-26 14:18:12 +0100

    updated backend accounts views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework_simplejwt.views import TokenRefreshView
 
-# Create your views here.
+User = get_user_model()
+
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
